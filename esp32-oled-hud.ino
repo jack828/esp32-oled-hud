b00718323b497afd316c8b6164c41bc451f84883
Jack Burgess <jackburgess124@gmail.com>
2019-12-22 00:26:50 +0000

    add device stats to frame 1

--- esp32-oled-hud.ino
+++ esp32-oled-hud.ino
@@ -16,6 +16,12 @@ SSD1306Wire display(0x3c, 5, 4);
 
 OLEDDisplayUi ui(&display);
 
+// Device
+String model = "";
+String firmwareVersion = "";
+String uptime = "";
+
+// Storage
 String capacity = "";
 String available = "";
 String used = "";
@@ -34,11 +40,12 @@ void titleOverlay(OLEDDisplay *display, OLEDDisplayUiState* state) {
 }
 
 void drawFrame1(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
-  state->userData = (void *) "WiFi";
-  // draw an xbm image.
-  // Please note that everything that should be transitioned
-  // needs to be drawn relative to x and y
-  display->drawXbm(x + 34, y + 14, WiFi_Logo_width, WiFi_Logo_height, WiFi_Logo_bits);
+  state->userData = (void *) "Device";
+  display->setTextAlignment(TEXT_ALIGN_LEFT);
+  display->setFont(Monospaced_plain_10);
+  display->drawString(0 + x, 10 + y, "Model:   " + model);
+  display->drawString(0 + x, 20 + y, "Firmware: " + firmwareVersion);
+  display->drawString(0 + x, 30 + y, "Uptime:   " + uptime);
 }
 
 void drawFrame2(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
@@ -159,13 +166,25 @@ boolean getCsrfToken () {
   return false;
 }
 
-double doubleFromXMLTag (String xml, String tag) {
+String stringFromXMLTag (String xml, String tag) {
   // TODO not found case
   String value = "";
   int start = xml.indexOf("<" + tag + ">") + tag.length() + 2;
   int end = xml.indexOf("</" + tag + ">", start);
   value = xml.substring(start, end);
+  return value;
+}
+
+double doubleFromXMLTag (String xml, String tag) {
+  // TODO not found case
+  String value = stringFromXMLTag(xml, tag);
   return value.toDouble();
+}
+
+long longFromXMLTag (String xml, String tag) {
+  // TODO not found case
+  String value = stringFromXMLTag(xml, tag);
+  return value.toInt();
 }
 
 String formatBytes(double bytes) {
@@ -179,7 +198,17 @@ String formatBytes(double bytes) {
   return output;
 }
 
-void getDeviceStats() {
+String formatUptime(long uptime) {
+  long dayInSeconds = 24 * 60 * 60;
+  long hourInSeconds = 60 * 60;
+  long minuteInSeconds = 60; // Obviously
+  int days = floor(uptime / dayInSeconds);
+  int hours = floor((uptime % dayInSeconds) / hourInSeconds);
+  int minutes = floor((uptime % hourInSeconds) / minuteInSeconds);
+  return String(days) + "d " + String(hours) + "h " + String(minutes) + "m";
+}
+
+void getStorageStats() {
   Serial.println("[HTTP] GET device stats");
 
   http.begin(READYNAS_DBBROKER_URI);
@@ -218,6 +247,38 @@ void getDeviceStats() {
   http.end();
 }
 
+void getDeviceStats() {
+  Serial.println("[HTTP] GET device stats");
+
+  http.begin(READYNAS_DBBROKER_URI);
+  http.setAuthorization(READYNAS_USERNAME, READYNAS_PASSWORD);
+  http.addHeader("csrfpid", csrfToken);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  int httpCode = http.POST("<?xml version=\"1.0\" encoding=\"UTF-8\"?><xs:nml xmlns:xs=\"http://www.netgear.com/protocol/transaction/NMLSchema-0.9\" xmlns=\"urn:netgear:nas:readynasd\" src=\"dpv_1576970265000\" dst=\"nas\"><xs:transaction id=\"njl_id_29\"><xs:get id=\"njl_id_28\" resource-id=\"SystemInfo\" resource-type=\"SystemInfo\"></xs:get></xs:transaction></xs:nml>");
+
+  if (httpCode > 0) {
+    Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+    if (httpCode == HTTP_CODE_OK) {
+      String payload = http.getString();
+      Serial.println(payload);
+      model = stringFromXMLTag(payload, "Model");
+      firmwareVersion = stringFromXMLTag(payload, "Firmware_Version");
+      long uptimeSeconds = longFromXMLTag(payload, "System_Uptime");
+
+      uptime = formatUptime(uptimeSeconds);
+
+      Serial.println(model);
+    } else {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+  } else {
+    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+  http.end();
+}
+
 void setup() {
   Serial.begin(115200);
   Serial.println("Booted.");
@@ -235,6 +296,7 @@ void setup() {
     // Authenticate with ReadyNAS & get device stats
     if (getCsrfToken()) {
       getDeviceStats();
+      getStorageStats();
     }
 
   } else {
